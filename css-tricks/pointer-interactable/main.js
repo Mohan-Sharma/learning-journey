f26c43b0d46ed6c444af1eddaa39bf7024c5d489
Mohan Sharma <mohan.sharma@sap.com>
2024-01-03 12:27:11 +0530

    css learnings

--- css-tricks/pointer-interactable/main.js
+++ css-tricks/pointer-interactable/main.js
@@ -1,15 +1,18 @@
 const blob = document.querySelector(".blob");
 
 document.body.addEventListener('mousemove', (e) => {
-    moveBlob(e);
+    const img = e.target.closest(".interactable__img"),
+            intersectingImg = img !== null;
+    moveBlob(e, intersectingImg);
 });
 
-const moveBlob = (e) => {
+const moveBlob = (e, intersecting) => {
     const clientX = e.clientX - blob.offsetWidth/2;
     const clientY = e.clientY - blob.offsetWidth/2;
     const keyframes = {
         left: `${clientX}px`,
-        top: `${clientY}px`
+        top: `${clientY}px`,
+        scale: `${intersecting ? 2.5 : 1}`
     };
 
     blob.animate(keyframes, { duration: 300, fill: "forwards" })
